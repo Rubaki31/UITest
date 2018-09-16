9769ab7b36f3c79741fd16d1f62c67cbe1a8a2bc
Minnigaliev <minnigaliev@bpcbt.com>
2018-09-16 17:49:00 +0300

    Добавил шаги на клики по яндексу и сайту

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -9,12 +9,20 @@ import static com.codeborne.selenide.Selenide.open;
     public class UITest {
         @Test
         public void yandexSearch() {
+            //WebDriver driver =new ChromeDriver();
+
             Selenide.clearBrowserCookies();
-            //Открываем яндекс
-            open("https://yandex.ru");
-            // Производим поиск
+            //Открываем Яндекс
+            open("https://www.yandex.ru");
+            //Вводим ЕЭТП в поле поиска и нажимаем Enter
             $(By.id("text")).val("ЕЭТП").pressEnter();
 
+            //$("#ires .g").shouldHave(text("Roseltorg.ru"));
+            $(By.xpath("//b[3]")).click();
+
+            $(By.cssSelector("a.g-register-btn.js-register-popup-link")).click();
+            $(By.cssSelector("img[alt=\"Поставщик\"]")).click();
+            $(By.xpath("xpath=//a[contains(text(),'Поставщик')]")).click();
 
         }
     }
