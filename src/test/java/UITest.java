6d76a174f1e93d29184cf84a0a86626d2795f36c
Minnigaliev <minnigaliev@bpcbt.com>
2018-09-17 09:54:10 +0300

    Добавил задержки на подгрузку страниц Исправил переход в новое окно

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,15 +1,20 @@
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.switchTo;
 
 
-    public class UITest {
+public class UITest {
         @Test
         public void yandexSearch() {
             //WebDriver driver =new ChromeDriver();
+            WebDriverRunner.isChrome();
 
             Selenide.clearBrowserCookies();
             //Открываем Яндекс
@@ -19,10 +24,25 @@ import static com.codeborne.selenide.Selenide.open;
 
             //$("#ires .g").shouldHave(text("Roseltorg.ru"));
             $(By.xpath("//b[3]")).click();
+            //Так как ссылка откроется в новом окне, переключаемся на него
+            switchTo().window(1);
+
+
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
             $(By.cssSelector("a.g-register-btn.js-register-popup-link")).click();
-            $(By.cssSelector("img[alt=\"Поставщик\"]")).click();
-            $(By.xpath("xpath=//a[contains(text(),'Поставщик')]")).click();
+            $(By.xpath("//a[2]/i/img")).click();
+            $(By.xpath("//div[2]/div/div/div/div/a")).click();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
         }
     }
