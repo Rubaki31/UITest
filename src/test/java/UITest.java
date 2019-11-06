ecbb7c4ec16e73fc30ead45b2851e85a2472f4f1
Minnigaliev <lokkol89@gmail.com>
2019-11-06 17:40:54 +0300

    1

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,12 +1,24 @@
 
 
 import org.junit.Test;
-
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class UITest {
-
+ @Test
+    public void NewTest () {
+     System.setProperty("webdriver.chrome.driver", "C:\\Users\\minnigaliev\\IdeaProjects2\\UITest\\chromedriver.exe");
+  ChromeOptions options = new ChromeOptions();
+  options.addArguments("start-maximized"); // open Browser in maximized mode
+  options.addArguments("disable-infobars"); // disabling infobars
+  options.addArguments("--disable-extensions"); // disabling extensions
+  options.addArguments("--disable-gpu"); // applicable to windows os only
+  options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+  options.addArguments("--no-sandbox"); // Bypass OS security mode
+     ChromeDriver driver = new ChromeDriver();
+     driver.get("https://yandex.ru");
+ }
 
 }
 
