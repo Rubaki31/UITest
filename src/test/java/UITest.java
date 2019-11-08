37d6117ab3846db96816424e9e4eb4b007618311
Minnigaliev <lokkol89@gmail.com>
2019-11-08 17:30:49 +0300

    Поиск плюс переход на сайт и ещё хромдрайвер

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -1,25 +1,36 @@
 
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class UITest {
- @Test
-    public void NewTest () {
-     System.setProperty("webdriver.chrome.driver", "C:\\Users\\minnigaliev\\IdeaProjects2\\UITest\\chromedriver.exe");
-  ChromeOptions options = new ChromeOptions();
-  options.addArguments("start-maximized"); // open Browser in maximized mode
-  options.addArguments("disable-infobars"); // disabling infobars
-  options.addArguments("--disable-extensions"); // disabling extensions
-  options.addArguments("--disable-gpu"); // applicable to windows os only
-  options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-  options.addArguments("--no-sandbox"); // Bypass OS security mode
-     ChromeDriver driver = new ChromeDriver();
-     driver.get("https://yandex.ru");
- }
+        public ChromeDriver driver;
+        @Before
+        public void SetUp () {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
 
+ @Test
+    public void test1 () {
+
+     driver.get("https://yandex.ru");
+     //driver.getMouse();
+     driver.findElement(By.id("text")).sendKeys("dtf");
+     driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+     driver.findElement(By.xpath("//a/div[2]")).click();
+     
+ }
+    @After
+    public void close() {
+            driver.quit();
+    }
 }
 
 
